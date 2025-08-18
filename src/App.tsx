74a31c9eb91639a200ff642c64f7e87878a05aaa
Siddhesh Dupare <siddheshdupare92@gmail.com>
2025-08-19 00:34:18 +0530

    Init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import Header from "@/components/Header.tsx"
+
+import './App.css'
+
+function App() {
+
+    return (
+        <>
+            <Header />
+        </>
+    );
+}
+
+export default App
